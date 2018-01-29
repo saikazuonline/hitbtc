bc1b680014412ddef637bf3fde90b9fa7edede4a
kazuki saito <kazuki@kazuki.local>
2018-01-30 06:45:41 +0900

    created token designation auto buy system

---
+++ src/autoBuyCHSB.py
@@ -0,0 +1,30 @@
+import hitBtc
+import configparser
+from module import *
+
+if __name__ == "__main__":
+    # START API情報
+    conf = configparser.ConfigParser()
+    conf.read('../info/setting.conf')
+    # publicKey
+    public_key = conf.get('INFO', 'API_KEY')
+    # secretKey
+    secret = conf.get('INFO', 'SECRET_KEY')
+    # END API情報
+
+    # 実行情報
+    client = hitBtc.Client("https://api.hitbtc.com", public_key, secret)
+
+    afCurrency = getAfterCurrencys(client)
+    beCurrency = textReadForCurrency()
+
+    currencys = comparison(afCurrency, beCurrency)
+
+    token = 'CHSB'
+
+    for currency in currencys:
+        if currency == token:
+            orderInfo = calculation(conf, client, currency)
+            buy(client, orderInfo)
+
+    textWriteForCurrency(getBalances(client))
